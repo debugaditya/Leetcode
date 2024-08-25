72d9fabf3acbb1c3356dfe58a840795c1558fbf6
Aditya <75940480+debugaditya@users.noreply.github.com>
2024-08-25 22:39:13 +0000

    [LeetCode Sync] Runtime - 8 ms (7.75%), Memory - 26.2 MB (100.00%)

---
+++ my-folder/3555-final-array-state-after-k-multiplication-operations-i/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> getFinalState(vector<int>& nums, int k, int multiplier) {
+        for(int i=1;i<=k;i++){
+        
+        auto min = min_element(nums.begin(), nums.end()); 
+        *min *= multiplier;
+        
+        
+        }
+        return nums;
+        
+    }
+};
