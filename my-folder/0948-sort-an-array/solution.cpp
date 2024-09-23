4bc546102fd39a663d9f9017244dd16997901c98
Aditya <75940480+debugaditya@users.noreply.github.com>
2024-09-23 12:03:35 +0000

    [LeetCode Sync] Runtime - 92 ms (63.53%), Memory - 70.5 MB (99.88%)

--- my-folder/0948-sort-an-array/solution.cpp
+++ my-folder/0948-sort-an-array/solution.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     vector<int> sortArray(vector<int>& nums) {
-        // Sort the vector in place
+        
         sort(nums.begin(), nums.end());
         
-        // Return the sorted vector
+        
         return nums;
     }
 };
