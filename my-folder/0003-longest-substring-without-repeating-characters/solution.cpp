668599883659ac7b01a2db12ac70535d623cd74b
Aditya <75940480+debugaditya@users.noreply.github.com>
2024-09-26 17:24:38 +0000

    [LeetCode Sync] Runtime - 18 ms (29.18%), Memory - 14.1 MB (35.15%)

--- my-folder/0003-longest-substring-without-repeating-characters/solution.cpp
+++ my-folder/0003-longest-substring-without-repeating-characters/solution.cpp
@@ -1,28 +1,35 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        vector<int> b;
-        for (int i = 0; i < s.size(); i++) {
-            set<char> a;
-            int num = 0;
-            for (int j = i; j < s.size(); j++) {
-                
-                if (a.find(s[j]) == a.end()) {
-                    a.insert(s[j]);
-                    num++;
-                } else {
-                    break;
-                }
-            }
-            b.push_back(num);
-        }
-        int max = 0;
-        for (int k = 0; k < b.size(); k++) {
-            if (b[k] > max) {
-                max = b[k];
+        vector<int> c; // Vector to store lengths of valid substrings
+        int a = 0; // Right pointer
+        int b = 0; // Left pointer
+        map<char, int> mp; // Map to store the last index of each character
+
+        while (a < s.size()) {
+            if (mp.find(s[a]) == mp.end()) {
+                // Character not seen before
+                mp[s[a]] = a; // Add character to map with its index
+                a++;
+            } else {
+                // Character is a duplicate
+                c.push_back(a - b); // Record length of current valid substring
+                b = max(b, mp[s[a]] + 1); // Move `b` to the right of the last occurrence of s[a]
+                mp[s[a]] = a; // Update last occurrence index
+                a++; // Move right pointer
             }
         }
-        return max;
+
+        // Capture the length of the last valid substring
+        c.push_back(a - b); // Ensure we capture the last substring if it’s valid
+
+        // Find maximum length from vector `c`
+        int max1 = 0; 
+        for (int i = 0; i < c.size(); i++) {
+            max1 = max(max1, c[i]); 
+        }
+        return max1; 
     }
 };
+
 
