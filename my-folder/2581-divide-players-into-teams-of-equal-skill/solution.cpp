7e77f4545545ddf8efe71bc4f8b1cb12710c1543
Aditya <75940480+debugaditya@users.noreply.github.com>
2024-10-04 13:52:22 +0000

    [LeetCode Sync] Runtime - 70 ms (5.15%), Memory - 56.5 MB (30.92%)

---
+++ my-folder/2581-divide-players-into-teams-of-equal-skill/solution.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    long long dividePlayers(vector<int >& skill) {
+        sort(skill.begin(),skill.end());
+        long long j=skill.size();
+        long long  d=skill[0]+skill[j-1];
+        long long  l=1;
+        long long  r=j-2;
+        long long  sum=skill[0]*skill[j-1];
+        while(l<r){
+            if(skill[l]+skill[r]==d){
+                sum+=skill[l]*skill[r];
+                l++;
+                r--;
+
+            }
+            else{
+                return -1;
+            }
+        }
+        return sum;
+
+        
+    }
+};
