805aca68d1aae3f29741c6a27fd756ea68ccb953
Aditya <75940480+debugaditya@users.noreply.github.com>
2024-10-07 01:59:00 +0000

    [LeetCode Sync] Runtime - 6 ms (18.61%), Memory - 10.1 MB (95.58%)

---
+++ my-folder/2800-minimum-string-length-after-removing-substrings/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minLength(string s) {
+        for(int i=0;i<s.size();i++){
+                if(s[i]=='A'&& s[i+1]=='B'||s[i]=='C'&& s[i+1]=='D'){
+                    s.erase(i,2);
+                    i=-1;
+                }
+            }
+        
+        
+            
+            
+            
+        
+        return s.size();
+        
+    }
+};
