5fe7542cc28b78d1fdb8479ddd5c189b55610a00
Aditya <75940480+debugaditya@users.noreply.github.com>
2024-10-14 13:54:26 +0000

    [LeetCode Sync] Runtime - 194 ms (5.34%), Memory - 75.9 MB (97.10%)

---
+++ my-folder/2616-maximal-score-after-applying-k-operations/solution.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+#include <queue>
+#include <cmath>
+using namespace std;
+
+class Solution {
+public:
+    long long maxKelements(vector<int>& nums, int k) {
+        priority_queue<int> pq(nums.begin(), nums.end());
+        long long ans = 0;
+        
+        for (int i = 0; i < k; ++i) {
+            int top = pq.top();
+            pq.pop();
+            ans += top;
+            pq.push(ceil(top / 3.0));
+        }
+        
+        return ans;
+    }
+};
+
