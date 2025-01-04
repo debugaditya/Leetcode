3172a3bc2272fa1c3b631f1d22f46de6ddc1bc93
Aditya <75940480+debugaditya@users.noreply.github.com>
2025-01-04 23:24:49 +0000

    [LeetCode Sync] Runtime - 383 ms (13.25%), Memory - 16.5 MB (20.00%)

---
+++ my-folder/2059-unique-length-3-palindromic-subsequences/solution.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+class Solution {
+public:
+    int countPalindromicSubsequence(string s) {
+        set<char>st;
+        map<char,pair<int,int>>mp;
+        int ans=0;
+        for(int i=0;i<s.size();i++){
+            if(st.find(s[i])==st.end()){
+                st.insert(s[i]);
+                mp[s[i]].first= i;
+            }
+            mp[s[i]].second=i;
+        }
+        for(auto it:mp){
+            int start = it.second.first;
+            int end = it.second.second;
+            if(end-start>1){
+                set<char> st2(s.begin() + start + 1, s.begin() + end);
+                ans+=st2.size();
+            }
+        }
+        return ans;
+    }
+};
