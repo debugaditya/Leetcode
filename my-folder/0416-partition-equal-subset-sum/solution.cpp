15d1739c4db64f44516eecb2dc8b4fb7c8e66a23
Aditya <75940480+debugaditya@users.noreply.github.com>
2025-01-18 05:57:08 +0000

    [LeetCode Sync] Runtime - 63 ms (88.05%), Memory - 100.5 MB (46.36%)

---
+++ my-folder/0416-partition-equal-subset-sum/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool f(vector<int>& nums,int sum,int index,int t,vector<vector<int>>&dp){
+        if(sum==t) return true;
+        if(index>=nums.size()||sum>t) return false;
+        if(dp[index][sum]!=-1) return dp[index][sum];
+        return dp[index][sum]=f(nums,sum+nums[index],index+1,t,dp)||f(nums,sum,index+1,t,dp);
+    }
+    bool canPartition(vector<int>& nums) {
+        int t= accumulate(nums.begin(),nums.end(),0);
+        if(t%2!=0) return false;
+        vector<vector<int>> dp(nums.size(), vector<int>(t / 2 + 1, -1));
+        return f(nums,0,0,t/2,dp);
+    }
+};
