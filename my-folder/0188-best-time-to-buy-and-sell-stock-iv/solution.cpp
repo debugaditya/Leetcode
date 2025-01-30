6f4dac38045fd94f544f93338f8a9124e15cfcf7
Aditya <75940480+debugaditya@users.noreply.github.com>
2025-01-30 06:37:58 +0000

    [LeetCode Sync] Runtime - 13 ms (33.50%), Memory - 18.3 MB (28.27%)

---
+++ my-folder/0188-best-time-to-buy-and-sell-stock-iv/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int f(vector<int>& prices,int k,int i1,int i2,int i3,vector<vector<vector<int>>>& dp){
+        if(i1>=prices.size()) return 0;
+        if(i3==k) return 0;
+        if(dp[i1][i2][i3]!=-1) return dp[i1][i2][i3];
+        if(i2==0) return dp[i1][i2][i3]=max(f(prices,k,i1+1,1,i3,dp)-prices[i1],f(prices,k,i1+1,0,i3,dp));
+        else return dp[i1][i2][i3]=max(f(prices,k,i1+1,1,i3,dp),f(prices,k,i1+1,0,i3+1,dp)+prices[i1]);
+        return 0;
+    }
+    int maxProfit(int k,vector<int>& prices) {
+        vector<vector<vector<int>>> dp(prices.size(), vector<vector<int>>(2, vector<int>(k+1, -1))); 
+        return f(prices,k,0,0,0,dp);
+    }
+};
