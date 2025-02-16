6d677e3ae2d783933f9753213b308ca344e82ec1
Aditya <75940480+debugaditya@users.noreply.github.com>
2025-02-16 21:13:19 +0000

    [LeetCode Sync] Runtime - 28 ms (32.23%), Memory - 10.9 MB (50.13%)

---
+++ my-folder/0647-palindromic-substrings/solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int countSubstrings(string s) {
+        int n = s.length(), count = 0;
+    vector<vector<bool>> dp(n, vector<bool>(n, false));
+
+    for (int gap = 0; gap < n; gap++) {
+        for (int i = 0, j = gap; j < n; i++, j++) {
+            if (gap == 0) {
+                dp[i][j] = true;
+            } else if (gap == 1) {
+                dp[i][j] = (s[i] == s[j]);
+            } else {
+                dp[i][j] = (s[i] == s[j] && dp[i+1][j-1]);
+            }
+            if (dp[i][j]) count++;
+        }
+    }
+    return count;
+    }
+};
