b847a500f6c38f1f9f6ede6df127a79c68684425
Aditya <75940480+debugaditya@users.noreply.github.com>
2025-02-18 14:13:41 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8.6 MB (5.13%)

--- my-folder/2509-minimize-xor/solution.cpp
+++ my-folder/2509-minimize-xor/solution.cpp
@@ -1,23 +1,55 @@
 class Solution {
 public:
-    int fn(int n){
+    int minimizeXor(int num1, int num2) {
+        int cnt=0;
         int c=0;
-        while(n) n&=n-1,c++;
-        return c;
-    }
-    int minimizeXor(int a, int b) {
-        int c=fn(b);
-        int x=0;
-        for(int i=29; i>=0 && c; i--){
-            if(a&(1<<i)) x^=(1<<i),c--;
-        }
-        if(c){
-            for(int i=0; i<30 && c; i++){
-                if(x&(1<<i)) continue;
-                x^=(1<<i);
-                c--;
+        vector<int>v;
+        int ans=0;
+        int cnt1=0;
+        int dup=num1;
+        while(num2>0||num1>0){
+            if(num2%2==1) cnt++;
+            num2/=2;
+            if(num1>0){
+                if(num1%2==1){
+                    v.push_back(1);
+                    cnt1++;
+                }
+                else v.push_back(0);
+                num1/=2;
             }
         }
-        return x;
+        if(cnt>v.size()){
+            for(int i=0;i<cnt;i++){
+                ans+=pow(2,i);
+            }
+        }
+        else if(cnt==cnt1) return dup;
+        else if(cnt<cnt1&&cnt<=v.size()){
+            for(int i=v.size()-1;i>=0;i--){
+                if(c==cnt) break;
+                if(v[i]==1){
+                    ans+=pow(2,i);
+                    c++;
+                } 
+            }
+        }
+        else if(cnt1<cnt&&cnt<=v.size()){
+            for(int i=v.size()-1;i>=0;i--){
+                if(c==cnt) break;
+                if(v[i]==1){
+                    ans+=pow(2,i);
+                    c++;
+                } 
+            }
+            for(int j=0;j<v.size();j++){
+                if(c==cnt) break;
+                if(v[j]==0){
+                    ans+=pow(2,j);
+                    c++;
+                } 
+            }
+        }
+        return ans;
     }
 };
