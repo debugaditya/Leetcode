ae633034fb0407a691aa0c37fff96080b6131c35
Aditya <75940480+debugaditya@users.noreply.github.com>
2025-03-01 20:58:19 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8.8 MB (80.83%)

--- my-folder/0067-add-binary/solution.cpp
+++ my-folder/0067-add-binary/solution.cpp
@@ -1,45 +1,20 @@
 class Solution {
 public:
     string addBinary(string a, string b) {
-        int carry=0;
-        int i1=a.size()-1;
-        int i2=b.size()-1;
-        string ans="";
-        while(i1>=0&&i2>=0){
-            if((a[i1]-'0')+(b[i2]-'0')+carry==0) ans.insert(ans.begin(),'0');
-            else if((a[i1]-'0')+(b[i2]-'0')+carry==1){
-                ans.insert(ans.begin(),'1');
-                carry=0;
-            }
-            else if((a[i1]-'0')+(b[i2]-'0')+carry==2){
-                ans.insert(ans.begin(),'0');
-                carry=1;
-            }
-            else if((a[i1]-'0')+(b[i2]-'0')+carry==3) ans.insert(ans.begin(),'1');     
-            i1--;
-            i2--;       
+        int carry = 0;
+        int i1 = a.size() - 1, i2 = b.size() - 1;
+        string ans = "";
+
+        while (i1 >= 0 || i2 >= 0 || carry) {
+            int sum = carry;
+            if (i1 >= 0) sum += a[i1--] - '0';
+            if (i2 >= 0) sum += b[i2--] - '0';
+
+            ans.insert(ans.begin(), (sum % 2) + '0'); 
+            carry = sum / 2; 
         }
-        while(i1>=0||i2>=0){
-            if(i1>=0){
-                if((a[i1]-'0')+carry==0) ans.insert(ans.begin(),'0');  
-                else if((a[i1]-'0')+carry==1){
-                    ans.insert(ans.begin(),'1');
-                    carry=0;
-                } 
-                else if((a[i1]-'0')+carry==2) ans.insert(ans.begin(),'0');
-            }
-            if(i2>=0){
-                if((b[i2]-'0')+carry==0) ans.insert(ans.begin(),'0');  
-                else if((b[i2]-'0')+carry==1){
-                    ans.insert(ans.begin(),'1');
-                    carry=0;
-                } 
-                else if((b[i2]-'0')+carry==2) ans.insert(ans.begin(),'0');
-            }
-            i1--;
-            i2--;
-        }
-        if(carry==1) ans.insert(ans.begin(),'1');
+
         return ans;
     }
 };
+
