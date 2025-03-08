79901eb4e2b5904bdd4fdbe616be0ce88bb778e1
Aditya <75940480+debugaditya@users.noreply.github.com>
2025-03-08 08:06:10 +0000

    [LeetCode Sync] Runtime - 860 ms (7.35%), Memory - 9.4 MB (79.37%)

--- my-folder/0005-longest-palindromic-substring/solution.cpp
+++ my-folder/0005-longest-palindromic-substring/solution.cpp
@@ -1,38 +1,30 @@
 class Solution {
+private: 
+    bool check(string &s, int i, int j){
+        while(i<j){
+            if(s[i] != s[j]){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }            
 public:
-    string t(int i,string s){
-        string ans(1, s[i]); 
-        int i1=i-1;
-        int i2=i+1;
-        while(i1>=0&&i2<s.size()&&s[i1]==s[i2]){
-            ans.push_back(s[i2]);
-            ans.insert(ans.begin(),s[i1]);
-            i1--;
-            i2++;
-        }
-        return ans;
-    }
-    string f(int i,string s){
-        string ans="";
-        int i1=i+1;
-        int i2=i;
-        while(i2>=0&&i1<s.size()&&s[i2]==s[i1]){
-            ans.push_back(s[i1]);
-            ans.insert(ans.begin(),s[i2]);
-            i2--;
-            i1++;
-        }
-        return ans;
-    }
-    int maxi=1;
     string longestPalindrome(string s) {
-        if(s.size()==1) return  s;
-        string ans="";
-        for(int i=1;i<s.size();i++){
-            if(t(i,s).size()>ans.size()) ans=t(i,s); 
-            if(f(i,s).size()>ans.size()) ans=f(i,s);  
+        int n = s.size();
+        int starting_index = 0;
+        int max_len = 0;
+        for(int i=0; i<n; i++){
+            for(int j=i; j<n; j++){
+                if(check(s, i, j)){
+                    if(j-i+1 > max_len){
+                        max_len = j-i+1;
+                        starting_index = i;
+                    }
+                }
+            }
         }
-        if(f(0,s).size()>ans.size()) ans=f(0,s);
-        return ans;
+        return s.substr(starting_index, max_len);
     }
-};
+}; 
