5d3a8ff49324218e5e908f4f6273e234e80aeaa6
Aditya <75940480+debugaditya@users.noreply.github.com>
2025-03-15 22:44:15 +0000

    [LeetCode Sync] Runtime - 3 ms (75.30%), Memory - 20.8 MB (9.72%)

--- my-folder/0207-course-schedule/solution.cpp
+++ my-folder/0207-course-schedule/solution.cpp
@@ -1,41 +1,28 @@
 class Solution {
 public:
-    bool canFinish(int n, vector<vector<int>>& p) {
-        vector<vector<int>> g(n);
-        vector<int> in_degree(n, 0);
-        
-        // Build adjacency list and in-degree array
-        for (auto it : p) {
-            g[it[1]].push_back(it[0]);
-            in_degree[it[0]]++;
-        }
-
-        queue<int> q;
-        vector<int> visited(n, 0);
-
-        // Start BFS from nodes with in-degree 0
-        for (int i = 0; i < n; i++) {
-            if (in_degree[i] == 0) {
+    bool canFinish(int c, vector<vector<int>>& p) {
+        vector<vector<int>>adj(c);
+        for(auto it:p) adj[it[1]].push_back(it[0]);
+        vector<int>indegree(adj.size(),0);
+        for(auto it:adj) for(auto it1:it) indegree[it1]++;
+        vector<int>topo;
+        queue<int>q;
+        for(int i=0;i<indegree.size();i++){
+            if(indegree[i]==0){
                 q.push(i);
-                visited[i] = 1;
             }
         }
-
-        int count = 0;
-        while (!q.empty()) {
-            int node = q.front();
+        while(!q.empty()){
+            topo.push_back(q.front());
+            int node=q.front();
             q.pop();
-            count++;
-            for (auto it : g[node]) {
-                if (--in_degree[it] == 0) {
+            for(auto it:adj[node]){
+                indegree[it]--;
+                if(indegree[it]==0){
                     q.push(it);
-                    visited[it] = 1;
                 }
             }
         }
-
-        // If all courses are visited, return true
-        return count == n;
+        return topo.size()==adj.size();
     }
 };
-
