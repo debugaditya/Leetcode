0a2ebe2743e5bb7a5720a7779252ccb77b0cc19a
Aditya <75940480+debugaditya@users.noreply.github.com>
2025-03-16 00:28:39 +0000

    [LeetCode Sync] Runtime - 55 ms (27.16%), Memory - 70.5 MB (5.47%)

--- my-folder/0820-find-eventual-safe-states/solution.cpp
+++ my-folder/0820-find-eventual-safe-states/solution.cpp
@@ -1,38 +1,29 @@
 class Solution {
 public:
-    int n;
-    vector<int> degreein;
-    vector< vector<int> > rGraph;
-    vector<int> ans;
-    
-    void init(vector<vector<int>>& G){
-        n = G.size();
-        degreein = vector<int>(n,0);
-        rGraph.resize(n, vector<int>(0) );
-        
-        for (int u=0; u<n; u++){
-            for (int v:G[u]){
-                rGraph[v].push_back(u);
-                degreein[u]++;
-            }
-        }
-    }
-    
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        init(graph);
-        queue<int> que;
-        for (int i=0; i<n; i++){
-            if (degreein[i]==0) que.push(i);
-        }
-        while (que.size()){
-            int u = que.front(); que.pop();
-            ans.push_back(u);
-            for (int v:rGraph[u]){
-                degreein[v]--;
-                if (degreein[v]==0) que.push(v);
+        vector<vector<int>>adj(graph.size());
+        vector<int>indegree(graph.size(),0);
+        for(int i=0;i<graph.size();i++) for(auto it:graph[i]) adj[it].push_back(i); 
+        for(auto it:adj) for(auto it1:it) indegree[it1]++;
+        vector<int>topo;
+        queue<int>q;
+        for(int i=0;i<indegree.size();i++){
+            if(indegree[i]==0){
+                q.push(i);
             }
         }
-        sort(ans.begin(),ans.end());
-        return ans;
+        while(!q.empty()){
+            topo.push_back(q.front());
+            int node=q.front();
+            q.pop();
+            for(auto it:adj[node]){
+                indegree[it]--;
+                if(indegree[it]==0){
+                    q.push(it);
+                }
+            }
+        }
+        sort(topo.begin(),topo.end());
+        return topo;
     }
 };
