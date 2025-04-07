99ee407f003c7ca0bae3d51bb2bc87f781462c57
Aditya <75940480+debugaditya@users.noreply.github.com>
2025-04-07 00:56:30 +0000

    [LeetCode Sync] Runtime - 51 ms (92.84%), Memory - 100.3 MB (46.36%)

--- my-folder/0416-partition-equal-subset-sum/solution.cpp
+++ my-folder/0416-partition-equal-subset-sum/solution.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     bool f(vector<int>& nums,int sum,int index,int t,vector<vector<int>>&dp){
         if(sum==t) return true;
+        if(sum>t) return false;
         if(index>=nums.size()||sum>t) return false;
         if(dp[index][sum]!=-1) return dp[index][sum];
         return dp[index][sum]=f(nums,sum+nums[index],index+1,t,dp)||f(nums,sum,index+1,t,dp);
