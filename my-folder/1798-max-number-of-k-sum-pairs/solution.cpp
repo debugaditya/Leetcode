d7eb1ee9dbb6401f6eecd3bf6a3a93d5d96e57fc
Aditya <75940480+debugaditya@users.noreply.github.com>
2025-04-17 22:22:09 +0000

    [LeetCode Sync] Runtime - 113 ms (29.23%), Memory - 76.8 MB (9.96%)

---
+++ my-folder/1798-max-number-of-k-sum-pairs/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxOperations(vector<int>& nums, int k) {
+        unordered_map<int,int>mp;
+        int ans=0;
+        for(auto it:nums){
+            if(mp[k-it]>0){
+                ans++;
+                mp[k-it]--;
+            }
+            else mp[it]++;
+        }
+        return ans;
+    }
+};
