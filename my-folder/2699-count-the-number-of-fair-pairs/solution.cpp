30e6aee433278b1da2dc9f79406e22b831a59792
Aditya <75940480+debugaditya@users.noreply.github.com>
2025-04-19 12:17:59 +0000

    [LeetCode Sync] Runtime - 52 ms (68.30%), Memory - 60.5 MB (100.00%)

--- my-folder/2699-count-the-number-of-fair-pairs/solution.cpp
+++ my-folder/2699-count-the-number-of-fair-pairs/solution.cpp
@@ -1,24 +1,37 @@
 class Solution {
 public:
-    long long countFairPairs(vector<int>& nums, int lower, int upper) {
-        long long ans = 0;
-        sort(nums.begin(), nums.end());
-        int n = nums.size();
-
-        for (int i = 0; i < n - 1; ++i) {
-            // Use two-pointer approach to find the range for nums[j]
-            int left = lower - nums[i];
-            int right = upper - nums[i];
-
-            // Find the first position where nums[j] >= left
-            int j = lower_bound(nums.begin() + i + 1, nums.end(), left) - nums.begin();
-            // Find the first position where nums[j] > right
-            int k = upper_bound(nums.begin() + i + 1, nums.end(), right) - nums.begin();
-
-            ans += (k - j);
+    int lower_bound(int n,vector<int>& nums){
+        int l=0;
+        int r=nums.size();
+        while(l<r){
+            int mid=(l+r)/2;
+            if(nums[mid]>=n) r=mid;
+            else l=mid+1;
         }
-
+        return l;
+    }
+    int upper_bound(int n,vector<int>& nums){
+        int l=0;
+        int r=nums.size();
+        while(l<r){
+            int mid=(l+r)/2;
+            if(nums[mid]<=n) l=mid+1;    
+            else r=mid;
+        }
+        return l;
+    }
+    long long countFairPairs(vector<int>& nums, int lower, int upper) {
+        long long ans=0;
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size();i++){
+            int j1=lower-nums[i];
+            int j2=upper-nums[i];
+            int lb = lower_bound(j1, nums);
+            int ub = upper_bound(j2, nums);
+            if (ub > i + 1) {
+                ans += (ub - max(lb, i + 1));
+            }
+        }
         return ans;
     }
 };
-
