136832b602f68a19c9bc8d4b619d8930edd944c1
Aditya <75940480+debugaditya@users.noreply.github.com>
2025-04-19 16:23:26 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 7.8 MB (76.65%)

---
+++ my-folder/0263-ugly-number/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isUgly(long n) {
+        if(n<=0) return false;
+        while(n>1){
+            if(n%2!=0&&n%3!=0&&n%5!=0) return false;
+            if(n%2==0) n/=2;
+            if(n%3==0) n/=3;
+            if(n%5==0) n/=5;
+        }
+        return true;
+    }
+};
