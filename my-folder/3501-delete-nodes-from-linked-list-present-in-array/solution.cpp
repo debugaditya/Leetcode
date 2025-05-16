4e9a5e145fecd843174e819eb162c026b08a79d0
Aditya <75940480+debugaditya@users.noreply.github.com>
2025-05-16 19:21:35 +0000

    [LeetCode Sync] Runtime - 90 ms (82.32%), Memory - 263.4 MB (35.49%)

--- my-folder/3501-delete-nodes-from-linked-list-present-in-array/solution.cpp
+++ my-folder/3501-delete-nodes-from-linked-list-present-in-array/solution.cpp
@@ -1,46 +1,20 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-
 class Solution {
 public:
     ListNode* modifiedList(vector<int>& nums, ListNode* head) {
-        map<int, int> mp;
-        for (auto i : nums) {
-            mp[i]++;
-        }
-        
-        ListNode* t = head;
-        ListNode* p = nullptr;
-        while (head != nullptr && mp.find(head->val) != mp.end()) {
-            ListNode* dl = head;
-            head = head->next;
-            delete(dl);
-        }
-        
-        t = head;  
-        
-        while (t != nullptr) {
-            if (mp.find(t->val) != mp.end()) {
-                ListNode* dl = t;
-                if (p != nullptr) {
-                    p->next = t->next;
-                }
-                t = t->next;
-                delete(dl);
-            } else {
-                p = t;
-                t = t->next;
+        unordered_set<int>st(nums.begin(),nums.end());
+        ListNode* dummy2=head;
+        ListNode* dummy1=new ListNode(-1,dummy2);
+        ListNode* ans=dummy1;
+        while(dummy2){
+            if(st.find(dummy2->val)!=st.end()){
+                dummy1->next=dummy2->next;
+                dummy2=dummy1->next;
+            }
+            else{
+                dummy1=dummy2;
+                dummy2=dummy2->next;
             }
         }
-        
-        return head;
+        return ans->next;
     }
 };
