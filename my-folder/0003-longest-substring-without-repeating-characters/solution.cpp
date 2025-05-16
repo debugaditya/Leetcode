b89b658f29d6c85a596682ef25a82f46ca8f6a9e
Aditya <75940480+debugaditya@users.noreply.github.com>
2025-05-16 20:16:02 +0000

    [LeetCode Sync] Runtime - 18 ms (29.18%), Memory - 14.1 MB (34.05%)

--- my-folder/0003-longest-substring-without-repeating-characters/solution.cpp
+++ my-folder/0003-longest-substring-without-repeating-characters/solution.cpp
@@ -1,35 +1,22 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        vector<int> c; // Vector to store lengths of valid substrings
-        int a = 0; // Right pointer
-        int b = 0; // Left pointer
-        map<char, int> mp; // Map to store the last index of each character
-
-        while (a < s.size()) {
-            if (mp.find(s[a]) == mp.end()) {
-                // Character not seen before
-                mp[s[a]] = a; // Add character to map with its index
-                a++;
-            } else {
-                // Character is a duplicate
-                c.push_back(a - b); // Record length of current valid substring
-                b = max(b, mp[s[a]] + 1); // Move `b` to the right of the last occurrence of s[a]
-                mp[s[a]] = a; // Update last occurrence index
-                a++; // Move right pointer
+        unordered_set<char>st;
+        int size=0,ans=0,l=0,r=0;
+        while(r<s.size()){
+            if(st.find(s[r])!=st.end()){
+                ans=max(size,ans);
+                st.erase(s[l]);
+                l++;
+                size--;
             }
-        }
-
-        // Capture the length of the last valid substring
-        c.push_back(a - b); // Ensure we capture the last substring if it’s valid
-
-        // Find maximum length from vector `c`
-        int max1 = 0; 
-        for (int i = 0; i < c.size(); i++) {
-            max1 = max(max1, c[i]); 
-        }
-        return max1; 
+            else{
+                st.insert(s[r]);
+                size++;
+                r++;
+            }
+        } 
+        ans=max(size,ans);      
+        return ans;
     }
 };
-
-
