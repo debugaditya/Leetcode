dd32210afb048992e18eaa48017ebbdd3cc319bf
Aditya <75940480+debugaditya@users.noreply.github.com>
2025-05-17 18:30:55 +0000

    [LeetCode Sync] Runtime - 99 ms (28.08%), Memory - 122.4 MB (23.11%)

---
+++ my-folder/2721-sum-of-distances/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<long long> distance(vector<int>& arr) {
+        unordered_map<int,long long>sum;
+        unordered_map<int,long long>freq;
+        vector<long long>ans(arr.size(),0);
+        for(int i=0;i<arr.size();i++){
+            ans[i]+=freq[arr[i]]*i-sum[arr[i]];
+            freq[arr[i]]++;
+            sum[arr[i]]+=i;
+        }
+        sum.clear(); freq.clear();
+        for(int i=arr.size()-1;i>=0;i--){
+            ans[i]+=sum[arr[i]]-freq[arr[i]]*i;
+            freq[arr[i]]++;
+            sum[arr[i]]+=i;
+        }
+        return ans;
+    }
+};
