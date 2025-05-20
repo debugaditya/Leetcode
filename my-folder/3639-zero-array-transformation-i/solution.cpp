148dda375efc4c7c809a8516418631496f29512c
Aditya <75940480+debugaditya@users.noreply.github.com>
2025-05-20 04:25:50 +0000

    [LeetCode Sync] Runtime - 12 ms (42.88%), Memory - 292.9 MB (67.12%)

--- my-folder/3639-zero-array-transformation-i/solution.cpp
+++ my-folder/3639-zero-array-transformation-i/solution.cpp
@@ -3,15 +3,14 @@ public:
     bool isZeroArray(vector<int>& nums, vector<vector<int>>& q) {
         vector<int>v(nums.size()+1,0);
         int sum=0;
-        for(auto it:q){
+        for(auto &it:q){
             v[it[0]]++;
             v[it[1]+1]--;
         }
-        for(int i=0;i<v.size();i++){
-            sum+=v[i];
-            v[i]=sum;
+        for(int i=0;i<nums.size();i++){
+            sum+=v[i]; 
+            if(sum<nums[i]) return false;
         }
-        for(int i=0;i<nums.size();i++) if(nums[i]>v[i]) return false;
         return true;
     }
 };
