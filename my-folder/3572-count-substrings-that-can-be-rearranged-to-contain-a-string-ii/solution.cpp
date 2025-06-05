16284f137db512d7db799633dd6823a57c20b3cb
Aditya <75940480+debugaditya@users.noreply.github.com>
2025-06-05 04:04:04 +0000

    [LeetCode Sync] Runtime - 60 ms (64.46%), Memory - 52.1 MB (73.49%)

---
+++ my-folder/3572-count-substrings-that-can-be-rearranged-to-contain-a-string-ii/solution.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    long long validSubstringCount(string w1, string w2) {
+        long long ans=0; long long cnt=0; int unique=0;
+        vector<int>mp1(26,0); vector<int>mp2(26,0);
+        for(auto it:w2) {if(mp2[it-'a']==0) unique++; mp2[it-'a']++;}
+        int l=0; int r=0; mp1[w1[0]-'a']=1;
+        if(mp2[w1[0]-'a']==1) cnt++;
+        while(r<w1.size()){
+            if(cnt>=unique){
+                ans+=w1.size()-r;
+                mp1[w1[l]-'a']--;
+                if(mp1[w1[l]-'a']<mp2[w1[l]-'a']) cnt--;
+                l++;
+            }
+            else{
+                r++;
+                if(r==w1.size()) break;
+                mp1[w1[r]-'a']++;
+                if(mp1[w1[r]-'a']==mp2[w1[r]-'a']) cnt++;
+            }
+        }
+        return ans;
+    }
+};
