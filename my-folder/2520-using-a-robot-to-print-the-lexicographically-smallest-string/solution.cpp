3feb26e02257dbd2d5ee1125bf697629ff889400
Aditya <75940480+debugaditya@users.noreply.github.com>
2025-06-06 21:15:09 +0000

    [LeetCode Sync] Runtime - 42 ms (73.71%), Memory - 34.2 MB (56.81%)

---
+++ my-folder/2520-using-a-robot-to-print-the-lexicographically-smallest-string/solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string robotWithString(string s) {
+        vector<char>mp(s.size()); char mini='z';
+        string t=""; string ans="";
+        for(int  i=s.size()-1;i>=0;i--){
+            mini=min(s[i],mini);
+            mp[i]=mini;
+        }
+        for(int i=0;i<s.size();i++){
+            if(t.size()>0&&t.back()<=mp[i]){
+                ans+=t.back(); t.pop_back();
+                i--;
+            }
+            else if(s[i]==mp[i]) ans+=s[i];
+            else t+=s[i];
+        }
+        reverse(t.begin(),t.end()); ans+=t;
+        return ans;
+    }
+};
