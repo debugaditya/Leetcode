6d9ef37fcb8439e551f03b1a37b68b63f448d6da
Aditya <75940480+debugaditya@users.noreply.github.com>
2025-06-10 00:19:54 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 9.2 MB (93.97%)

--- my-folder/3753-maximum-difference-between-even-and-odd-frequency-i/solution.cpp
+++ my-folder/3753-maximum-difference-between-even-and-odd-frequency-i/solution.cpp
@@ -1,14 +1,9 @@
 class Solution {
 public:
-    int maxDifference(string s) {
-        vector<int>mp(26,0);
-        int odd=INT_MIN;
-        int even1=INT_MAX;
-        for(int i=0;i<s.size();i++) mp[(s[i]-'a')]++;
-        for(int i=0;i<mp.size();i++){
-            if(mp[i]%2!=0&&mp[i]!=0) odd=max(odd,mp[i]);
-            if(mp[i]%2==0&&mp[i]!=0) even1=min(even1,mp[i]);  
-        }
-        return odd-even1;
+    int maxDifference(string s){
+        int maxi=0,mini=INT_MAX; vector<int>mp(26,0);
+        for(auto it:s) mp[it-'a']++;
+        for(auto it:mp) {if(it%2==1||it==0) maxi=max(maxi,it);else mini=min(mini,it);}
+        return maxi-mini;
     }
 };
