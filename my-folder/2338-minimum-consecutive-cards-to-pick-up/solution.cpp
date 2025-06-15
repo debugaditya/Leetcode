1c11af5bd7ae9c00f49daa6e0791329f5224f2d6
Aditya <75940480+debugaditya@users.noreply.github.com>
2025-06-15 16:25:03 +0000

    [LeetCode Sync] Runtime - 124 ms (80.11%), Memory - 119.1 MB (36.27%)

--- my-folder/2338-minimum-consecutive-cards-to-pick-up/solution.cpp
+++ my-folder/2338-minimum-consecutive-cards-to-pick-up/solution.cpp
@@ -2,10 +2,9 @@ class Solution {
 public:
     int minimumCardPickup(vector<int>& c) {
         int mini=INT_MAX;
-        int maxi=*max_element(c.begin(),c.end());
-        vector<int>mp(maxi+1,-1);
+        unordered_map<int,int>mp;
         for(int i=0;i<c.size();i++){
-            if(mp[c[i]]!=-1) mini=min(mini,i-mp[c[i]]+1);
+            if(mp.count(c[i])) mini=min(mini,i-mp[c[i]]+1);
             mp[c[i]]=i;
         }
         return mini==INT_MAX?-1:mini;
