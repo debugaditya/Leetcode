677c0d8f35460418354e40bde64a1e19ac69b143
Aditya <75940480+debugaditya@users.noreply.github.com>
2025-06-16 09:35:23 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 12 MB (30.87%)

---
+++ my-folder/2144-maximum-difference-between-increasing-elements/solution.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int maximumDifference(vector<int>& nums) {
+        int mini=nums[0]; int ans=INT_MIN;
+        for(int i=1;i<nums.size();i++){
+            ans=max(ans,nums[i]-mini);
+            mini=min(mini,nums[i]);
+        }
+        return ans>0?ans:-1;
+    }
+};
