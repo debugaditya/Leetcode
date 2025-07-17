1cabf5cabe58f1bc1283d0f0dbeb2a9a64f4cc56
Aditya <75940480+debugaditya@users.noreply.github.com>
2025-07-17 00:23:27 +0000

    [LeetCode Sync] Runtime - 2620 ms (5.22%), Memory - 90.3 MB (90.76%)

---
+++ my-folder/3491-find-the-maximum-length-of-valid-subsequence-ii/solution.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int maximumLength(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<vector<int>> dp(n, vector<int>(k, 0));
+        int res = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int len = 1 + go(j, (nums[i] + nums[j]) % k, dp, nums, k);
+                if (len > res) {
+                    res = len;
+                }
+            }
+        }
+
+        return res;
+    }
+
+private:
+    int go(int i, int rem, vector<vector<int>>& dp, vector<int>& nums, int k) {
+        if (dp[i][rem] != 0) {
+            return dp[i][rem];
+        }
+
+        int res = 1;
+        for (int j = i + 1; j < nums.size(); j++) {
+            if ((nums[j] + nums[i]) % k == rem) {
+                res += go(j, rem, dp, nums, k);
+                break;
+            }
+        }
+        dp[i][rem] = res;
+        return res;
+    }
+};
