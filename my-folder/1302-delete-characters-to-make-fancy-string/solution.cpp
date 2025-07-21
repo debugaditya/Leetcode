8959bccb38682f0bae303f01870908a83fbac967
Aditya <75940480+debugaditya@users.noreply.github.com>
2025-07-21 03:15:13 +0000

    [LeetCode Sync] Runtime - 28 ms (54.62%), Memory - 44.4 MB (84.14%)

--- my-folder/1302-delete-characters-to-make-fancy-string/solution.cpp
+++ my-folder/1302-delete-characters-to-make-fancy-string/solution.cpp
@@ -1,27 +1,12 @@
 class Solution {
 public:
     string makeFancyString(string s) {
-        char x=s[0];
-        int cnt=1;
-        string ans="";
-        ans+=s[0];
+        int cnt=1; string ans=""; ans+=s[0];
         for(int i=1;i<s.size();i++){
-            if(x==s[i]){
-                cnt++;
-                if(cnt>=3){
-                    continue;
-                }
-                else{
-                    ans+=s[i];
-                }
-            }
-            else{
-                x=s[i];
-                ans+=s[i];
-                cnt=1;
-            }
+            if(s[i]==s[i-1]) cnt++;
+            else cnt=1;
+            if(cnt<3) ans+=s[i];
         }
         return ans;
-        
     }
 };
