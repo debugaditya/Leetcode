2572819e59363288f40c873bded837c7b28adfc3
Aditya <75940480+debugaditya@users.noreply.github.com>
2025-08-15 10:35:17 +0000

    [LeetCode Sync] Runtime - 9 ms (54.55%), Memory - 24 MB (60.05%)

---
+++ my-folder/1782-smallest-string-with-a-given-numeric-value/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string getSmallestString(int n, int k) {
+        string ans(n,'a'); k-=n; int i=n-1;
+        while(k>0){
+            ans[i]=min(k,25)+'a';
+            k-=min(k,25);
+            i--;
+        }
+        return ans;
+    }
+};
