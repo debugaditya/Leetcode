0c1834e218f418e22c6e5436aae2a61cc346f363
Aditya <75940480+debugaditya@users.noreply.github.com>
2025-09-02 06:05:34 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8.5 MB (7.68%)

---
+++ my-folder/1252-break-a-palindrome/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string breakPalindrome(string palindrome) {
+        if(palindrome.size()==1) return "";
+        for(int i=0;i<palindrome.size();i++){
+            if(palindrome.size()%2==1&&i==palindrome.size()/2) continue;
+            if(palindrome[i]!='a'){palindrome[i]='a'; return palindrome;}
+        }
+        palindrome.back()='b';
+        return palindrome;
+    }
+};
