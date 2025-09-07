948cc174138f53e2039d7929b72737425c788dff
Aditya <75940480+debugaditya@users.noreply.github.com>
2025-09-07 01:06:54 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 9.5 MB (86.12%)

--- my-folder/1426-find-n-unique-integers-sum-up-to-zero/solution.cpp
+++ my-folder/1426-find-n-unique-integers-sum-up-to-zero/solution.cpp
@@ -5,7 +5,7 @@ public:
         if(n%2==1) ans.push_back(0);
         for(int i=1;i<=n/2;i++){
             ans.push_back(i);
-            ans.push_back(-1*i);
+            ans.push_back(-i);
         }
         return ans;
     }
