478a36f1e0dd559285ccb6c9ef1c92d96b23a4ed
Aditya <75940480+debugaditya@users.noreply.github.com>
2025-09-15 00:46:02 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 15.7 MB (35.88%)

--- my-folder/0031-next-permutation/solution.cpp
+++ my-folder/0031-next-permutation/solution.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int i = nums.size() - 2;
-        while (i >= 0 && nums[i] >= nums[i+1]) {
-            i--;
+        int idx=-1;
+        for(int i=nums.size()-2;i>=0;i--) if(nums[i]<nums[i+1]){idx=i; break;}
+        reverse(nums.begin()+idx+1,nums.end());
+        if(idx==-1) return;
+        int l=idx+1,r=nums.size()-1;
+        while(l<=r){
+            int mid=(l+r)/2;
+            if(nums[mid]>nums[idx]) r=mid-1;
+            else l=mid+1;
         }
-        if (i >= 0) {
-            int j = nums.size() - 1;
-            while (nums[j] <= nums[i]) {
-                j--;
-            }
-            swap(nums[i], nums[j]);
-        }
-        reverse(nums.begin() + i + 1, nums.end());
+        swap(nums[idx],nums[l]);
+        return;
     }
 };
-
