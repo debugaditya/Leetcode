17535c875c8a9e3c081d48d229f0ccfb465b8d25
Aditya <75940480+debugaditya@users.noreply.github.com>
2025-10-03 11:33:06 +0000

    [LeetCode Sync] Runtime - 39 ms (43.21%), Memory - 87.9 MB (93.52%)

---
+++ my-folder/2655-rearrange-array-to-maximize-prefix-score/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxScore(vector<int>& nums) {
+        sort(nums.begin(),nums.end()); long long sum=0,cnt=0;
+        for(int i=nums.size()-1;i>=0;i--){
+            sum+=nums[i];
+            if(sum>0) cnt++;
+            else break;
+        }
+        return cnt;
+    }
+};
