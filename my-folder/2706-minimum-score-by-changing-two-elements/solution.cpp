2d6281840a3b996a91e8dfcaae7b9c7a524e443e
Aditya <75940480+debugaditya@users.noreply.github.com>
2025-10-24 05:29:35 +0000

    [LeetCode Sync] Runtime - 16 ms (44.95%), Memory - 37.9 MB (92.20%)

---
+++ my-folder/2706-minimum-score-by-changing-two-elements/solution.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    int minimizeSum(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        return min({nums[nums.size()-2]-nums[1],nums[nums.size()-3]-nums[0],nums.back()-nums[2]});
+    }
+};
