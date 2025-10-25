b98c6deafa5b33bf3814636bcc0a6de501aace22
Aditya <75940480+debugaditya@users.noreply.github.com>
2025-10-25 00:50:12 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 7.7 MB (87.20%)

--- my-folder/1817-calculate-money-in-leetcode-bank/solution.cpp
+++ my-folder/1817-calculate-money-in-leetcode-bank/solution.cpp
@@ -1,9 +1,7 @@
 class Solution {
 public:
     int totalMoney(int n) {
-        int deposit=1;
-        int ans=0;
-        int dmon=1;
+        int deposit=1,ans=0,dmon=1;
         for(int i=1;i<=n;i++){
             ans+=deposit;
             deposit++;
