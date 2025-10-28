491a6972119ce98cf2171feb62d2bdac9b48c22b
Aditya <75940480+debugaditya@users.noreply.github.com>
2025-10-28 00:14:06 +0000

    [LeetCode Sync] Runtime - 3 ms (76.23%), Memory - 21.9 MB (82.06%)

--- my-folder/3616-make-array-elements-equal-to-zero/solution.cpp
+++ my-folder/3616-make-array-elements-equal-to-zero/solution.cpp
@@ -1,18 +1,9 @@
 class Solution {
 public:
     int countValidSelections(vector<int>& nums) {
-        int sum=0;
-        int ans=0;
-        map<int,int>mp;
-        for(int i=0;i<nums.size();i++){
-            sum+=nums[i];
-            if(nums[i]==0) mp[i]=sum;
-        }
-        for(auto it:mp){
-            if(2*it.second==sum) ans+=2;
-            if(2*it.second==sum+1) ans++;
-            if(2*it.second==sum-1) ans++;
-        }
+        int sum=0,ans=0,cnt=0; 
+        for(auto &it:nums) sum+=it;
+        for(auto &it:nums) {cnt+=it; if(it==0&&2*cnt==sum) ans+=2; else if(it==0&&abs(sum-2*cnt)==1) ans++;}
         return ans;
     }
 };
