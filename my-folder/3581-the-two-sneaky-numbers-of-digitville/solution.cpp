96e6a53f38201d9924b5c0cf842538ba2fff1a37
Aditya <75940480+debugaditya@users.noreply.github.com>
2025-10-31 01:41:55 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 26 MB (58.21%)

--- my-folder/3581-the-two-sneaky-numbers-of-digitville/solution.cpp
+++ my-folder/3581-the-two-sneaky-numbers-of-digitville/solution.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     vector<int> getSneakyNumbers(vector<int>& nums) {
-        vector<int>mp(nums.size(),0);
-        vector<int>ans;
+        vector<int>mp(nums.size(),0),ans;
         for(auto it:nums){
             mp[it]++;
             if(mp[it]==2) ans.push_back(it);
