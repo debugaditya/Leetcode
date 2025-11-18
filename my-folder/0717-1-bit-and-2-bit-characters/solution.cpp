3b5cd5c6bd82bde90821ddf29c41a68421f4bcd5
Aditya <75940480+debugaditya@users.noreply.github.com>
2025-11-18 00:10:44 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 13.2 MB (12.25%)

--- my-folder/0717-1-bit-and-2-bit-characters/solution.cpp
+++ my-folder/0717-1-bit-and-2-bit-characters/solution.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool isOneBitCharacter(vector<int>& bits) {
         for(int i=0;i<bits.size();i++){
-            if(i==bits.size()-1) return bits[i]==0;
+            if(i==bits.size()-1) return true;
             if(bits[i]==1) i++;
         }
         return false;
