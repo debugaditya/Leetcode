535ec307f87f736924611ec6c02a6345b9ce7a3e
Aditya <75940480+debugaditya@users.noreply.github.com>
2025-11-19 18:59:18 +0000

    [LeetCode Sync] Runtime - 7 ms (43.33%), Memory - 92.8 MB (5.96%)

---
+++ my-folder/3965-earliest-finish-time-for-land-and-water-rides-i/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int earliestFinishTime(vector<int>& lt, vector<int>& ld, vector<int>& wt, vector<int>& wd) {
+        vector<int>wet,let; int ans=INT_MAX;
+        for(int i=0;i<lt.size();i++) let.push_back(lt[i]+ld[i]); sort(let.begin(),let.end());
+        for(int i=0;i<wt.size();i++) wet.push_back(wt[i]+wd[i]); sort(wet.begin(),wet.end());
+        for(int i=0;i<lt.size();i++) if(wet[0]<=lt[i]) ans=min(ans,lt[i]+ld[i]);
+        for(int i=0;i<wt.size();i++) if(let[0]<=wt[i]) ans=min(ans,wt[i]+wd[i]);
+        for(int i=0;i<lt.size();i++) if(lt[i]<=wet[0]) ans=min(ans,wet[0]+ld[i]);
+        for(int i=0;i<wt.size();i++) if(wt[i]<=let[0]) ans=min(ans,let[0]+wd[i]); 
+        return ans;
+    }
+};
