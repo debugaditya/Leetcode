a1b23ac60ce29cf7d14d58931fbabd1fe7379693
Aditya <75940480+debugaditya@users.noreply.github.com>
2025-11-29 00:06:37 +0000

    [LeetCode Sync] Runtime - 63 ms (34.08%), Memory - 104.8 MB (28.67%)

--- my-folder/4075-count-subarrays-with-majority-element-ii/solution.cpp
+++ my-folder/4075-count-subarrays-with-majority-element-ii/solution.cpp
@@ -1,19 +1,48 @@
 class Solution {
 public:
-    long long findnumberofelementsmallerthen(int sum, vector<int>& mp) {
-        return upper_bound(mp.begin(), mp.end(), sum - 1) - mp.begin();
-    }
     long long countMajoritySubarrays(vector<int>& nums, int target) {
-        long long ans = 0;
-        vector<int> mp;  
-        int sum = 0;
-        mp.push_back(0); 
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] == target) sum++;
-            else sum--;
-            ans += findnumberofelementsmallerthen(sum, mp);
-            mp.insert(upper_bound(mp.begin(), mp.end(), sum), sum);
+        int n = nums.size();
+        vector<long long> pref(n+1, 0);
+
+        // Build prefix sum of +1/-1 transform
+        for(int i = 0; i < n; i++){
+            pref[i+1] = pref[i] + (nums[i] == target ? 1 : -1);
         }
+
+        // Coordinate compress
+        vector<long long> comp = pref;
+        sort(comp.begin(), comp.end());
+        comp.erase(unique(comp.begin(), comp.end()), comp.end());
+        
+        auto getIndex = [&](long long x){
+            return lower_bound(comp.begin(), comp.end(), x) - comp.begin() + 1;
+        };
+
+        int m = comp.size();
+        vector<long long> BIT(m+1, 0);
+
+        auto update = [&](int i){
+            for(; i <= m; i += i & -i) BIT[i]++;
+        };
+        auto query = [&](int i){
+            long long res = 0;
+            for(; i > 0; i -= i & -i) res += BIT[i];
+            return res;
+        };
+
+        long long ans = 0;
+
+        // Count number of previous prefix < current prefix
+        for(int i = 0; i <= n; i++){
+            int idx = getIndex(pref[i]);
+
+            // count all prefix < current → query(idx-1)
+            ans += query(idx - 1);
+
+            // insert current
+            update(idx);
+        }
+
         return ans;
     }
 };
