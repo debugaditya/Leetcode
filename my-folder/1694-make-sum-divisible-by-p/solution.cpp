9f0f92a082ab4684f9094cee291aeca034619751
Aditya <75940480+debugaditya@users.noreply.github.com>
2025-11-30 00:52:05 +0000

    [LeetCode Sync] Runtime - 76 ms (67.99%), Memory - 91.8 MB (13.05%)

--- my-folder/1694-make-sum-divisible-by-p/solution.cpp
+++ my-folder/1694-make-sum-divisible-by-p/solution.cpp
@@ -1,37 +1,18 @@
-class Solution { 
- public:
-  int minSubarray(vector<int>& nums, int p) {
-    long long sum = 0;
-    vector<long long> m; // Keep the same variable name as in your original code
-
-    map<long long, long long> mp2;
-    for (long long i = 0; i < nums.size(); i++) {
-      sum += nums[i];
+class Solution {
+public:
+    int minSubarray(vector<int>& nums, int p) {
+        vector<int>mp(nums.size()+1); mp[nums.size()]=0; long long sum=0; int ans=nums.size();
+        unordered_map<int,int>mp2; mp2[0]=-1;
+        for(int i=nums.size()-1;i>=0;i--){
+            sum+=nums[i];
+            mp[i]=sum%p;
+        } sum=0;
+        for(int i=0;i<nums.size();i++){
+            sum+=nums[i];
+            int mod=(p-mp[i+1])%p;
+            mp2[sum%p]=i;
+            if(mp2.count(mod)) ans=min(i-mp2[mod],ans);
+        }
+        return ans==nums.size()?-1:ans;
     }
-    
-    long long target = sum % p; // Change variable name to target
-    if (target == 0) {
-      return 0;
-    }
-
-    mp2[0] = -1; // To handle cases where a subarray from the start has the needed mod
-    long long sum2 = 0;
-    int mini = nums.size(); // Change minLength to mini
-
-    for (long long j = 0; j < nums.size(); j++) {
-      sum2 += nums[j];
-      long long currentMod = sum2 % p;
-      long long neededMod = (currentMod - target + p) % p;
-
-      if (mp2.find(neededMod) != mp2.end()) {
-        mini = min(mini, static_cast<int>(j - mp2[neededMod])); // Change minLength to mini
-      }
-
-      // Update mp2 with the current prefix sum modulo
-      mp2[currentMod] = j;
-    }
-
-    return mini == nums.size() ? -1 : mini; // Change minLength to mini
-  }
 };
-
