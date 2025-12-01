ff6871671016cabdbb004c706725fef3ce5bf5e3
Aditya <75940480+debugaditya@users.noreply.github.com>
2025-12-01 23:09:25 +0000

    [LeetCode Sync] Runtime - 449 ms (6.94%), Memory - 46.8 MB (8.54%)

--- my-folder/4074-count-subarrays-with-majority-element-i/solution.cpp
+++ my-folder/4074-count-subarrays-with-majority-element-i/solution.cpp
@@ -6,7 +6,11 @@ public:
             mp[i]=mp[i-1];
             if(nums[i]==target) mp[i]++;
         }
-        for(int i=0;i<nums.size();i++) for(int j=i;j<nums.size();j++) if(2*(mp[j]-mp[i-1])>j-i+1) ans++;
+        for(int i=0;i<nums.size();i++){
+            for(int j=i;j<nums.size();j++){
+                if(2*(mp[j]-mp[i-1])>j-i+1) ans++;
+            }
+        }
         return ans;
     }
 };
