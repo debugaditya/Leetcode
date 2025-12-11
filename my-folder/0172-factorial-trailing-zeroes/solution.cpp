f42fc3c9292d72cc6d9cee9c85494c3068fb807c
Aditya <75940480+debugaditya@users.noreply.github.com>
2025-12-11 23:05:01 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8.1 MB (50.87%)

---
+++ my-folder/0172-factorial-trailing-zeroes/solution.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int trailingZeroes(int n) {
+        int ans=0,power=5;
+        while(n/power>0){
+            ans+=n/power;
+            power*=5;
+        }
+        return ans;
+    }
+};
