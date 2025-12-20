7fad668c6329d73a9afd48abab80183621d3ef86
Aditya <75940480+debugaditya@users.noreply.github.com>
2025-12-20 14:50:22 +0000

    [LeetCode Sync] Runtime - 315 ms (33.33%), Memory - 257.2 MB (21.26%)

---
+++ my-folder/4162-minimum-number-of-operations-to-have-distinct-elements/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        unordered_map<int,int>mp;  int cnt=0;
+        for(auto it:nums){
+            mp[it]++;
+            if(mp[it]==2) cnt++;
+        }
+        if(cnt==0) return 0;
+        for(int i=0;i<nums.size();i++){
+            int it=nums[i];
+            mp[it]--;
+            if(mp[it]==1) cnt--;
+            if(cnt==0){
+                return ceil(1.0*(i+1)/3);
+            }
+        }
+        return -1;
+    }
+};
