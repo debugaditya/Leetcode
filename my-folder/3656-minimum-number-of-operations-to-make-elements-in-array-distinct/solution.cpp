cfb3fb5302d388c7ed5296277eadf7b24ea3512f
Aditya <75940480+debugaditya@users.noreply.github.com>
2025-12-20 16:47:42 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 28.3 MB (42.97%)

--- my-folder/3656-minimum-number-of-operations-to-make-elements-in-array-distinct/solution.cpp
+++ my-folder/3656-minimum-number-of-operations-to-make-elements-in-array-distinct/solution.cpp
@@ -1,15 +1,20 @@
 class Solution {
 public:
     int minimumOperations(vector<int>& nums) {
-        vector<int>mp(101,0);
-        int ind=-1;
-        for(int i=nums.size()-1;i>=0;i--){
-            mp[nums[i]]++;
-            if(mp[nums[i]]==2){
-                ind=i+1;
-                break;
+        unordered_map<int,int>mp;  int cnt=0;
+        for(auto it:nums){
+            mp[it]++;
+            if(mp[it]==2) cnt++;
+        }
+        if(cnt==0) return 0;
+        for(int i=0;i<nums.size();i++){
+            int it=nums[i];
+            mp[it]--;
+            if(mp[it]==1) cnt--;
+            if(cnt==0){
+                return ceil(1.0*(i+1)/3);
             }
         }
-        return ceil(ind/3.0);
+        return -1;
     }
 };
