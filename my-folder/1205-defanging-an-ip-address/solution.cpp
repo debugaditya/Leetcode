a2f8573011f1c588878ea9a80fc27a33e3bb0d4a
Aditya <75940480+debugaditya@users.noreply.github.com>
2025-12-21 13:52:10 +0000

    [LeetCode Sync] Runtime - 3 ms (26.02%), Memory - 7.7 MB (75.85%)

---
+++ my-folder/1205-defanging-an-ip-address/solution.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    string defangIPaddr(string address) {
+        string ans="";
+        for(auto it:address){
+            if(it=='.'){ans+='['; ans+='.'; ans+=']';}
+            else ans+=it;
+        }
+        return ans;
+    }
+};
