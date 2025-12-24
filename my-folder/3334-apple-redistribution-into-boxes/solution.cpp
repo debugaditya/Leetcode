5add3962fbba9270e86f40267e23182aff1896d6
Aditya <75940480+debugaditya@users.noreply.github.com>
2025-12-24 00:55:44 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 33 MB (76.45%)

---
+++ my-folder/3334-apple-redistribution-into-boxes/solution.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int minimumBoxes(vector<int>& apple, vector<int>& capacity) {
+        sort(capacity.begin(),capacity.end()); int sum=accumulate(apple.begin(),apple.end(),0);
+        for(int i=capacity.size()-1;i>=0;i--){
+            sum-=capacity[i];
+            if(sum<=0) return capacity.size()-i;
+        }
+        return -1;
+    }
+};
