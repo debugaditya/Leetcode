c24bd0ba06d1e3aca22010657482b3c56df0f4bf
Aditya <75940480+debugaditya@users.noreply.github.com>
2026-01-09 00:23:08 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 16.1 MB (92.98%)

--- my-folder/0896-smallest-subtree-with-all-the-deepest-nodes/solution.cpp
+++ my-folder/0896-smallest-subtree-with-all-the-deepest-nodes/solution.cpp
@@ -1,45 +1,26 @@
 class Solution {
 public:
+    int cnt=0;
     void depth(TreeNode* root,int dep,int &ans){
         if(!root) return;
+        if(dep>ans) cnt=1; if(dep==ans) cnt++; 
         ans=max(ans,dep);
         depth(root->left,dep+1,ans);
         depth(root->right,dep+1,ans);
-        return;
     }
-    void seti(TreeNode* root,set<TreeNode* >&st,int ans,int depth){
-        if(!root) return;
-        if(depth==ans) st.insert(root);
-        seti(root->left,st,ans,depth+1);
-        seti(root->right,st,ans,depth+1);
-        return;
-    }
-    int f(TreeNode* root,set<TreeNode* >&st,TreeNode* &ans,bool &d){
-        if(!root) return 0;
-        if(d) return 0;
-        if(st.find(root)!=st.end()&&st.size()>1) return 1;
-        if(st.find(root)!=st.end()&&st.size()==1){
-            ans=root;
-            d=true;
-            return 0;
-        }
-        int left=f(root->left,st,ans,d);
-        int right=f(root->right,st,ans,d);
-        if(left+right==st.size()){
-            ans=root;
-            d=true;
-            return 0;
-        }
+    int f(TreeNode* root,int depth,int dep,TreeNode* &ans){
+        if(!root||ans!=nullptr) return 0;
+        if(depth==dep){if(cnt==1) ans=root; return cnt==1?0:1;}
+        int left=f(root->left,depth+1,dep,ans);
+        int right=f(root->right,depth+1,dep,ans);
+        if(left+right==cnt) {ans=root; return 0;}
         return left+right;
     }
     TreeNode* subtreeWithAllDeepest(TreeNode* root) {
-        int dep=0;
-        set<TreeNode*>st;
-        bool d=false;
-        TreeNode* ans=nullptr;
+        if(!root->left&&!root->right) return root;
+        int dep=0; TreeNode* ans=nullptr;
         depth(root,0,dep);
-        seti(root,st,dep,0);
-        f(root,st,ans,d);
+        f(root,0,dep,ans);
         return ans;
     }
 };
