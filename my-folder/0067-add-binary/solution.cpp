0bd5a741c20cd438a737e355f36b318022bce7aa
Aditya <75940480+debugaditya@users.noreply.github.com>
2026-02-15 21:44:48 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8.8 MB (97.11%)

--- my-folder/0067-add-binary/solution.cpp
+++ my-folder/0067-add-binary/solution.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
     string addBinary(string a, string b) {
-        int carry = 0;
-        int i1 = a.size() - 1, i2 = b.size() - 1;
-        string ans = "";
-
-        while (i1 >= 0 || i2 >= 0 || carry) {
-            int sum = carry;
-            if (i1 >= 0) sum += a[i1--] - '0';
-            if (i2 >= 0) sum += b[i2--] - '0';
-
-            ans.insert(ans.begin(), (sum % 2) + '0'); 
-            carry = sum / 2; 
+        int i=a.size()-1,j=b.size()-1,carry=0; string ans="";
+        while(i>=0||j>=0){
+            int sum=carry;
+            if(i>=0) sum+=a[i]-'0';
+            if(j>=0) sum+=b[j]-'0';
+            ans+=sum%2+'0'; carry=sum/=2;
+            i--; j--;
         }
-
+        if(carry==1) ans+='1';
+        reverse(ans.begin(),ans.end());
         return ans;
     }
 };
-
