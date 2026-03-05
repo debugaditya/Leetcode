24bf98666467f9aa189f72b8f3082a1cc933cc77
Aditya <75940480+debugaditya@users.noreply.github.com>
2026-03-05 00:15:44 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 9.5 MB (50.14%)

--- my-folder/1884-minimum-changes-to-make-alternating-binary-string/solution.cpp
+++ my-folder/1884-minimum-changes-to-make-alternating-binary-string/solution.cpp
@@ -1,11 +1,8 @@
 class Solution {
 public:
     int minOperations(string s) {
-        int cnt1=0,cnt2=0;
-        for(int i=0;i<s.size();i++){
-            if(s[i]==i%2+'0') cnt1++;
-            else cnt2++;
-        }
-        return min(cnt1,cnt2);
+        int cnt=0,n=s.size();
+        for(int i=0;i<s.size();i++) cnt+=abs(i%2-(s[i]-'0'));
+        return min(cnt,n-cnt);
     }
 };
