08aaf1928e133c99ce2e04359cad3891aed49d8d
Aditya <75940480+debugaditya@users.noreply.github.com>
2026-03-15 19:12:10 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8.6 MB (70.15%)

--- my-folder/0050-powx-n/solution.cpp
+++ my-folder/0050-powx-n/solution.cpp
@@ -1,7 +1,9 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        return pow(x,n);
-        
+        if(n==1) return x; if(n==0||x==1) return 1;
+        double ans=myPow(x,abs(n/2));
+        ans*=ans; if(n%2) ans*=x;
+        return n<0?1.0/ans:ans;
     }
 };
