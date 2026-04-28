40d1c32b7a2dab26a6e7da2a2bb20cb086141c5c
Aditya <75940480+debugaditya@users.noreply.github.com>
2026-04-28 21:45:24 +0000

    [LeetCode Sync] Runtime - 63 ms (7.56%), Memory - 99.9 MB (5.35%)

--- my-folder/2160-minimum-operations-to-make-a-uni-value-grid/solution.cpp
+++ my-folder/2160-minimum-operations-to-make-a-uni-value-grid/solution.cpp
@@ -1,43 +1,14 @@
 class Solution {
 public:
-    int minOperations(vector<vector<int>>& grid, int x) {
-        int n = grid.size();
-        int m = grid[0].size();
-        
-        priority_queue<int, vector<int>, greater<int>> minh;
-        vector<int> arr;
-
-        // Insert all elements into min-heap and store them in arr
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                minh.push(grid[i][j]);
-                arr.push_back(grid[i][j]);
-            }
-        }
-
-        int size = n * m;
-        int count = 0, median = 0;
-
-        // Extract the median element using the heap
-        while (count <= size / 2) {
-            median = minh.top();
-            minh.pop();
-            count++;
-        }
-
-        int remainder = grid[0][0] % x;
-        int ans = 0;
-
-        // Compute minimum operations
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (grid[i][j] % x != remainder) {
-                    return -1;
-                }
-                ans += abs(median - grid[i][j]) / x;
-            }
-        }
+    int f(int num,vector<int>& v, int x){
+        int ans=0;
+        for(auto it:v) {if(abs(it-num)%x!=0) return -1; ans+=abs(it-num)/x;}
         return ans;
     }
+    int minOperations(vector<vector<int>>& grid, int x) {
+        vector<int>v;
+        for(auto it:grid) for(auto it2:it) v.push_back(it2);
+        sort(v.begin(),v.end());
+        return f(v[v.size()/2],v,x);
+    }
 };
-
