1b994ea8cbd35826550cb0e871fea496322791ae
Aditya <75940480+debugaditya@users.noreply.github.com>
2026-04-30 00:15:16 +0000

    [LeetCode Sync] Runtime - 906 ms (39.82%), Memory - 499.5 MB (28.54%)

--- my-folder/3986-maximum-path-score-in-a-grid/solution.cpp
+++ my-folder/3986-maximum-path-score-in-a-grid/solution.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
-    int f(vector<vector<int>>& grid,int i,int j,int k,vector<vector<vector<int>>>&dp){
-        if(i==grid.size()||j==grid[0].size()||k<0) return -1;
-        if(i==grid.size()-1&&j==grid[0].size()-1){
-            if(grid.back().back()!=0&&k==0) return -1;
-            return grid.back().back();
-        }
+    int f(vector<vector<int>>& grid, int i,int j,int k,vector<vector<vector<int>>>& dp){
+        if(k<0||i==grid.size()||j==grid[0].size()) return -1;
+        if(i==grid.size()-1&&j==grid[0].size()-1&&k-(grid[i][j]>0?1:0)>=0) return grid[i][j];
         if(dp[i][j][k]!=-2) return dp[i][j][k];
-        if(grid[i][j]!=0) k--;
-        int right=f(grid,i,j+1,k,dp); int down=f(grid,i+1,j,k,dp);
-        if(grid[i][j]!=0) k++;
-        return dp[i][j][k]=max(right,down)==-1?-1:grid[i][j]+max(right,down);
+        int right=f(grid,i,j+1,k-(grid[i][j]>0?1:0),dp);
+        int down=f(grid,i+1,j,k-(grid[i][j]>0?1:0),dp);
+        int ans=max(right,down);
+        if(ans>=0) ans+=grid[i][j];
+        return dp[i][j][k]=ans;
     }
     int maxPathScore(vector<vector<int>>& grid, int k) {
-        int m=grid.size(),n=grid[0].size();
-        vector<vector<vector<int>>>dp(grid.size(),vector<vector<int>>(grid[0].size(),vector<int>(min(k+1,m+n),-2)));
-        return f(grid,0,0,min(k,m+n-1),dp);
+        vector<vector<vector<int>>>dp(grid.size(),vector<vector<int>>(grid[0].size(),vector<int>(k+1,-2)));
+        return f(grid,0,0,k,dp);
     }
 };
