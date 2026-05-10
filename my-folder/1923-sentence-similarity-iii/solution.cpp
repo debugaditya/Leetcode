90916aa1e29d7af0acf839423b2f888b6b8bad87
Aditya <75940480+debugaditya@users.noreply.github.com>
2026-05-10 01:22:49 +0000

    [LeetCode Sync] Runtime - 3 ms (18.36%), Memory - 8.7 MB (88.52%)

--- my-folder/1923-sentence-similarity-iii/solution.cpp
+++ my-folder/1923-sentence-similarity-iii/solution.cpp
@@ -1,48 +1,27 @@
 class Solution {
 public:
-    bool areSentencesSimilar(string sentence1, string sentence2) {
-        vector<string> s1, s2;
-        int l = 0, m = 0;
-        
-        // Split sentence1 into words
-        for (int i = 0; i < sentence1.size(); i++) {
-            if (sentence1[i] == ' ') {
-                s1.push_back(string(sentence1.begin() + l, sentence1.begin() + i));
-                l = i + 1;
+    bool areSentencesSimilar(string s1, string s2) {
+        string word=""; 
+        vector<string>v1,v2; int i=0;
+        for(auto it:s1){
+            if(it==' ') {
+                v1.push_back(word);
+                word="";
             }
+            else word+=it;
         }
-        s1.push_back(string(sentence1.begin() + l, sentence1.end()));  // Last word
-        
-        // Split sentence2 into words
-        for (int j = 0; j < sentence2.size(); j++) {
-            if (sentence2[j] == ' ') {
-                s2.push_back(string(sentence2.begin() + m, sentence2.begin() + j));
-                m = j + 1;
+        v1.push_back(word); word="";
+        for(auto it:s2){
+            if(it==' ') {
+                v2.push_back(word);
+                word="";
             }
+            else word+=it;
         }
-        s2.push_back(string(sentence2.begin() + m, sentence2.end()));  // Last word
-        
-        // Ensure s1 is the larger vector
-        if (s1.size() < s2.size()) {
-            swap(s1, s2);  // s1 should always be the larger or equal size
-        }
-        
-        // Matching prefix
-        int l2 = 0;                
-        while (l2 < s2.size() && s1[l2] == s2[l2]) {
-            l2++;
-        }
-        
-        // Matching suffix
-        int r = s1.size() - 1;      // End of s1
-        int r2 = s2.size() - 1;     // End of s2
-        while (r2 >= l2 && s1[r] == s2[r2]) {
-            r--;
-            r2--;
-        }
-        
-        // If we covered the entire smaller sentence (s2) between prefix and suffix
-        return (r2 < l2);
+        v2.push_back(word);
+        int mini=min((int)v1.size(),(int)v2.size());
+        while(v1[i]==v2[i]){i++; mini--; if(mini==0) return true;} i=1;
+        while(v1[v1.size()-i]==v2[v2.size()-i]){i++; mini--; if(mini==0) return true;}
+        return false;
     }
 };
-
