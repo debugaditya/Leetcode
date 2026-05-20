e920707a0238fbf804d2189f91306c7b6b675d3f
Aditya <75940480+debugaditya@users.noreply.github.com>
2026-05-20 02:30:34 +0000

    [LeetCode Sync] Runtime - 16 ms (20.53%), Memory - 88.4 MB (20.56%)

--- my-folder/2766-find-the-prefix-common-array-of-two-arrays/solution.cpp
+++ my-folder/2766-find-the-prefix-common-array-of-two-arrays/solution.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
-        set<int>st;
+        unordered_set<int>st;
         int cnt=0;
         vector<int>ans;
         for(int i=0;i<A.size();i++){
