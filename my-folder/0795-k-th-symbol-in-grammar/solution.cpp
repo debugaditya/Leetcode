27475b8583fb7249b57f6cad2ae4094c3b091bf5
Aditya <75940480+debugaditya@users.noreply.github.com>
2026-06-16 02:45:08 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 7.8 MB (82.90%)

--- my-folder/0795-k-th-symbol-in-grammar/solution.cpp
+++ my-folder/0795-k-th-symbol-in-grammar/solution.cpp
@@ -1,6 +1,16 @@
 class Solution {
 public:
+    long long power(int mid){
+        if(mid==0) return 1;
+        long long temp=power(mid/2);
+        long long ans=temp*temp;
+        if(mid%2) ans*=2;
+        return ans;
+    }
     int kthGrammar(int n, int k) {
-        return __builtin_popcount(k - 1) % 2;
+        if(n==1) return 0;
+        int len=power(n-1);
+        if(k>len/2) return 1-kthGrammar(n-1,k-len/2);
+        return kthGrammar(n-1,k);
     }
 };
