5d6dd1e6eab28972cc29ffa2b38f2d1e37f34c43
Aditya <75940480+debugaditya@users.noreply.github.com>
2026-06-18 02:33:25 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8.1 MB (70.97%)

--- my-folder/1446-angle-between-hands-of-a-clock/solution.cpp
+++ my-folder/1446-angle-between-hands-of-a-clock/solution.cpp
@@ -1,10 +1,6 @@
 class Solution {
 public:
     double angleClock(int hour, int minutes) {
-        double ans=0;
-        ans+=6*minutes;
-        ans-=1.0*((1.0*minutes/60)*30+1.0*30*(hour%12));
-        ans=abs(ans);
-        return min(ans,(double) 360-ans);
+        return min(abs(6*minutes-1.0*((1.0*minutes/60)*30+1.0*30*(hour%12))),360-abs(6*minutes-1.0*((1.0*minutes/60)*30+1.0*30*(hour%12))));
     }
 };
