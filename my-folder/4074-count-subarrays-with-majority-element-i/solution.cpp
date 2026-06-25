4ba8aef35ac8b2a3a4e84e85fbc0d9c2ebd6c2fe
Aditya <75940480+debugaditya@users.noreply.github.com>
2026-06-25 08:05:52 +0000

    [LeetCode Sync] Runtime - 51 ms (26.64%), Memory - 39.6 MB (72.55%)

--- my-folder/4074-count-subarrays-with-majority-element-i/solution.cpp
+++ my-folder/4074-count-subarrays-with-majority-element-i/solution.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     int countMajoritySubarrays(vector<int>& nums, int target) {
-        unordered_map<int,int>mp; int ans=0;
+        int ans=0;
         for(int i=0;i<nums.size();i++){
-            mp[i]=mp[i-1];
-            if(nums[i]==target) mp[i]++;
-        }
-        for(int i=0;i<nums.size();i++){
+            int cnt=0;
             for(int j=i;j<nums.size();j++){
-                if(2*(mp[j]-mp[i-1])>j-i+1) ans++;
+                if(nums[j]==target) cnt++;
+                if(2*cnt>j-i+1) ans++;
             }
         }
         return ans;
