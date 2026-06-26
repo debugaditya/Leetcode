2d95c6c8219ac7a0cd759aad5eff34798a2a714b
Aditya <75940480+debugaditya@users.noreply.github.com>
2026-06-26 00:50:33 +0000

    [LeetCode Sync] Runtime - 1203 ms (5.06%), Memory - 176.8 MB (8.69%)

--- my-folder/4075-count-subarrays-with-majority-element-ii/solution.cpp
+++ my-folder/4075-count-subarrays-with-majority-element-ii/solution.cpp
@@ -1,22 +1,29 @@
 class Solution {
 public:
-    long long findnumberofelementsmallerthen(int sum, vector<int>& mp) {
-        return upper_bound(mp.begin(), mp.end(), sum - 1) - mp.begin();
+    unordered_map<long long,long long>mp;
+    void update(long long i,int l,int r,int sum){
+        if(l==r) {mp[i]++; return;}
+        long long mid = l + ((long long)r - l) / 2;
+        if(mid<sum) update(2*i+2,mid+1,r,sum);
+        else update(2*i+1,l,mid,sum);
+        mp[i]=mp[2*i+1]+mp[2*i+2];
     }
-
+    long long get(long long i,int l,int r,int left,int right){
+        if(left>r||right<l) return 0;
+        if(left<=l&&r<=right) return mp[i];
+        if(l==r) return mp[i];
+        long long mid = l + ((long long)r - l) / 2;
+        return get(2*i+1,l,mid,left,right)+get(2*i+2,mid+1,r,left,right);
+    }
     long long countMajoritySubarrays(vector<int>& nums, int target) {
-        long long ans = 0;
-        vector<int> mp;  
-        int sum = 0;
-        mp.push_back(0); 
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] == target) sum++;
+        int sum=0; long long ans=0;
+        update(0,INT_MIN,INT_MAX,sum);
+        for (int i=0;i<nums.size();i++) {
+            if (nums[i]==target) sum++;
             else sum--;
-            ans += findnumberofelementsmallerthen(sum, mp);
-            mp.insert(upper_bound(mp.begin(), mp.end(), sum), sum);
+            ans+=get(0,INT_MIN,INT_MAX,INT_MIN,sum-1);
+            update(0,INT_MIN,INT_MAX,sum);
         }
-
         return ans;
     }
 };
-
