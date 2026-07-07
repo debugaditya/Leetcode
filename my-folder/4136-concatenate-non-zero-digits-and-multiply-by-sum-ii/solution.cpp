3b80f59927540f4a0b2cc2c8cc826f0f227d2c6b
Aditya <75940480+debugaditya@users.noreply.github.com>
2026-07-07 01:57:42 +0000

    [LeetCode Sync] Runtime - 847 ms (5.18%), Memory - 443.1 MB (5.18%)

--- my-folder/4136-concatenate-non-zero-digits-and-multiply-by-sum-ii/solution.cpp
+++ my-folder/4136-concatenate-non-zero-digits-and-multiply-by-sum-ii/solution.cpp
@@ -1,39 +1,28 @@
 class Solution {
 public:
-    const long long MOD = 1000000007;
-
-    long long modpow(long long a, long long b){
-        long long r = 1;
-        while(b > 0){
-            if(b & 1) r = (r * a) % MOD;
-            a = (a * a) % MOD;
-            b >>= 1;
-        }
-        return r;
+    int mod=1e9+7;
+    long long power(int p){
+        if(p==0) return 1;
+        long long a=power(p/2);
+        a%=mod; a*=a; a%=mod;
+        if(p%2) a*=10;
+        return a%mod;
     }
-
     vector<int> sumAndMultiply(string s, vector<vector<int>>& queries) {
-        unordered_map<int,long long> sum, num, cnt;
-        sum[-1] = 0; cnt[-1] = 0; num[-1] = 0;
-        vector<int> ans;
-
-        for(int i = 0; i < s.size(); i++){
-            sum[i] = (sum[i-1] + s[i] - '0') % MOD;
-            num[i] = (s[i]=='0') ? num[i-1] : (num[i-1] * 10 + (s[i]-'0')) % MOD;
-            cnt[i] = cnt[i-1] + (s[i] == '0' ? 0 : 1);
+        unordered_map<int,long long>mp,num,cnt; long long n=0,c=0; mp[-1]=0; num[-1]=0;
+        vector<int>v;
+        for(int i=0;i<s.size();i++){
+            mp[i]=mp[i-1]+(s[i]-'0');
+            if(s[i]!='0'){n*=10; n+=s[i]-'0'; n%=mod; c++;}
+            num[i]=n; cnt[i]=c;
         }
-
-        for(auto &it : queries){
-            long long L = it[0], R = it[1];
-            long long totalNum = num[R];
-            long long leftNum = num[L-1];
-            long long p = modpow(10, cnt[R] - cnt[L-1]);
-            long long seg = (totalNum - (leftNum * p) % MOD + MOD) % MOD;
-            long long ss = (sum[R] - sum[L-1] + MOD) % MOD;
-
-            ans.push_back( (ss * seg) % MOD );
+        for(auto it:queries){
+            long long sum=mp[it[1]]-mp[it[0]-1];
+            long long nm=(1ll*num[it[1]]-1LL*num[it[0]-1]*power(cnt[it[1]]-cnt[it[0]-1]))%mod;
+            nm+=mod; nm%=mod;
+            long long ans=nm*sum; ans%=mod;
+            v.push_back(ans);   
         }
-        return ans;
+        return v;
     }
 };
-
