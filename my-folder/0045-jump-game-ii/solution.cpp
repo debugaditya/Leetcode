10e0d127c5a8e401e623909201f85c7f7a584898
Aditya <75940480+debugaditya@users.noreply.github.com>
2026-07-18 14:13:20 +0000

    [LeetCode Sync] Runtime - 123 ms (13.73%), Memory - 22.1 MB (11.48%)

--- my-folder/0045-jump-game-ii/solution.cpp
+++ my-folder/0045-jump-game-ii/solution.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        queue<pair<int,int>>q; q.push({0,0}); int n=nums.size(); vector<int>dp(n,INT_MAX); dp[0]=0;
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>q; 
+        q.push({0,0}); int n=nums.size(); vector<int>dp(n,INT_MAX); dp[0]=0;
         while(!q.empty()){
-            auto [i,jump]=q.front(); q.pop();
+            auto [jump,i]=q.top(); q.pop();
             if(i==n-1) return jump;
-            for(int j=i+1;j<=min(n-1,i+nums[i]);j++) if(dp[j]>jump+1) {dp[j]=jump+1; q.push({j,jump+1});}
+            for(int j=i+1;j<=min(n-1,i+nums[i]);j++) if(dp[j]>jump+1) {dp[j]=jump+1; q.push({jump+1,j});}
         }
         return -1;
     }
