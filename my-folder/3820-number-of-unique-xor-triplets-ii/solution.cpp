4738a2f0fa8282574cd75344eb5525a1838b0728
Aditya <75940480+debugaditya@users.noreply.github.com>
2026-07-23 01:12:25 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 98.4 MB (40.94%)

--- my-folder/3820-number-of-unique-xor-triplets-ii/solution.cpp
+++ my-folder/3820-number-of-unique-xor-triplets-ii/solution.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
     int uniqueXorTriplets(vector<int>& nums) {
-        unordered_set<int>st,st1;
+        unordered_set<int>st,ans; 
         for(auto &it:nums) for(auto &it1:nums) st.insert(it^it1);
-        for(auto &it:nums) for(auto &it1:st) st1.insert(it^it1);
-        return st1.size();   
+        for(auto &it:st) for(auto &it1:nums) ans.insert(it^it1);
+        return ans.size();
     }
 };
 auto init = atexit([]() { ofstream("display_runtime.txt") << "0"; });
+
