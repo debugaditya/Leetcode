9210f6bfb733c24c6e861988b1e56061e20e6b63
Aditya <75940480+debugaditya@users.noreply.github.com>
2026-07-23 00:46:08 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 198.9 MB (60.63%)

--- my-folder/3824-number-of-unique-xor-triplets-i/solution.cpp
+++ my-folder/3824-number-of-unique-xor-triplets-i/solution.cpp
@@ -1,14 +1,11 @@
 class Solution {
 public:
+    int f(int n){
+        return pow(2,(int)log2(n)+1);
+    }
     int uniqueXorTriplets(vector<int>& nums) {
         int n=nums.size();
-        if(n==1) return 1; if(n==2) return 2;
-        int l=0,r=32;
-        while(l<=r){
-            int mid=(l+r)/2;
-            if(pow(2,mid)>n) r=mid-1;
-            else l=mid+1;
-        }
-        return pow(2,l);
+        if(n<=2) return n;
+        return f(n);
     }
 };
