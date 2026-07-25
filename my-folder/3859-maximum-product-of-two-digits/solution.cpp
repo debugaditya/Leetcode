84b809ad93a21d085d2850691927ba6660667378
Aditya <75940480+debugaditya@users.noreply.github.com>
2026-07-25 00:04:36 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8.5 MB (99.06%)

---
+++ my-folder/3859-maximum-product-of-two-digits/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxProduct(int n) {
+        int maxi1=0,maxi2=0;
+        while(n>0){
+            if(n%10>maxi1){
+                maxi2=maxi1;
+                maxi1=n%10;
+            }
+            else if(n%10>maxi2) maxi2=n%10;
+            n/=10;
+        }
+        return maxi1*maxi2;
+    }
+};
