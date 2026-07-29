0e3952c9c0db4639c1fc8f39120ddae4898ed896
Aditya <75940480+debugaditya@users.noreply.github.com>
2026-07-29 02:47:22 +0000

    [LeetCode Sync] Runtime - 19 ms (14.71%), Memory - 12.6 MB (78.55%)

--- my-folder/0368-largest-divisible-subset/solution.cpp
+++ my-folder/0368-largest-divisible-subset/solution.cpp
@@ -1,31 +1,30 @@
 class Solution {
 public:
+    int f(int i,vector<int>&mp,vector<int>&dp,vector<int>& nums){
+        if(i==nums.size()) return 0;
+        if(dp[i]!=-1) return dp[i];
+        int take=1,next=-1;
+        for(int j=i+1;j<nums.size();j++){
+            if(nums[j]%nums[i]==0){
+                int temp=1+f(j,mp,dp,nums);
+                if(take<temp) {take=temp; next=j;}
+            }
+        }
+        mp[i]=next;
+        return dp[i]=take;
+    }
     vector<int> largestDivisibleSubset(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        int n = nums.size();
-        vector<int> dp(n, 1), parent(n, -1);
-        int maxSize = 0, maxIndex = -1;
-
-        for (int i = 0; i < n; ++i){
-            for (int j = 0; j < i; ++j){
-                if (nums[i] % nums[j] == 0 && dp[j] + 1 > dp[i]){
-                    dp[i] = dp[j] + 1;
-                    parent[i] = j;
-                }
-            }
-            if (dp[i] > maxSize){
-                maxSize = dp[i];
-                maxIndex = i;
-            }
+        vector<int>mp(nums.size(),-1),dp(nums.size(),-1),ans;
+        sort(nums.begin(),nums.end());
+        int maxi=0,start=-1; 
+        for(int i=0;i<dp.size();i++){
+            int len=f(i,mp,dp,nums);
+            if(len>maxi){start=i; maxi=len;}
         }
-
-        vector<int> result;
-        while (maxIndex != -1){
-            result.push_back(nums[maxIndex]);
-            maxIndex = parent[maxIndex];
+        while(start>=0){
+            ans.push_back(nums[start]);
+            start=mp[start];
         }
-        reverse(result.begin(), result.end());
-        return result;
+        return ans;
     }
 };
-
