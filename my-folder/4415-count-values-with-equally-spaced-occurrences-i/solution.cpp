bb32ce8bbb063acb6f74649e70d15eb286b1ed96
Aditya <75940480+debugaditya@users.noreply.github.com>
2026-09-14 13:40:25 +0000

    [LeetCode Sync] Runtime - 27 ms (10.00%), Memory - 34.3 MB (50.00%)

---
+++ my-folder/4415-count-values-with-equally-spaced-occurrences-i/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int countSpecialIntegers(vector<int>& nums) {
+        unordered_map<int,int>diff,prev,mp;
+        unordered_set<int>st(nums.begin(),nums.end());
+        for(int i=0;i<nums.size();i++){
+            mp[nums[i]]++;
+            if(!st.count(nums[i])) continue;
+            else if(!prev.count(nums[i])) prev[nums[i]]=i;
+            else if(!diff.count(nums[i])) diff[nums[i]]=i-prev[nums[i]];
+            else if(i-prev[nums[i]]!=diff[nums[i]]) st.erase(nums[i]);
+            prev[nums[i]]=i;
+        }
+        if(st.size()==0) return 0;
+        int ans=st.size();
+        for(auto it:st) if(mp[it]!=3) ans--;
+        return ans;
+    }
+};
